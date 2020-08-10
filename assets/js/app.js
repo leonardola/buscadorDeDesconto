64a14b858b0e87fc946954cc156ec90a15b5090f
leonardo albuquerque <albuquerque.leonardo@trezo.com.br>
2020-08-10 13:28:29 -0300

    agora o js é carregado usando o yarn

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,22 +11,55 @@ import '../css/app.css';
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
-import { BrowserQRCodeReader } from '@zxing/library';
+import {BrowserMultiFormatReader, NotFoundException} from "@zxing/library";
 
-const codeReader = new BrowserQRCodeReader();
+window.addEventListener('load', function () {
+    let selectedDeviceId;
+    const codeReader = new BrowserMultiFormatReader()
+    console.log('ZXing code reader initialized')
+    codeReader.listVideoInputDevices()
+        .then((videoInputDevices) => {
+            const sourceSelect = document.getElementById('sourceSelect')
+            selectedDeviceId = videoInputDevices[0].deviceId
+            if (videoInputDevices.length >= 1) {
+                videoInputDevices.forEach((element) => {
+                    const sourceOption = document.createElement('option')
+                    sourceOption.text = element.label
+                    sourceOption.value = element.deviceId
+                    sourceSelect.appendChild(sourceOption)
+                })
 
-codeReader
-  .listVideoInputDevices()
-  .then(videoInputDevices => {
-    videoInputDevices.forEach(device =>
-      console.log(`${device.label}, ${device.deviceId}`)
-    );
-  })
-  .catch(err => console.error(err));
+                sourceSelect.onchange = () => {
+                    selectedDeviceId = sourceSelect.value;
+                };
 
- const firstDeviceId = videoInputDevices[0].deviceId;
+                const sourceSelectPanel = document.getElementById('sourceSelectPanel')
+                sourceSelectPanel.style.display = 'block'
+            }
 
-codeReader
-  .decodeOnceFromVideoDevice(firstDeviceId, 'video')
-  .then(result => console.log(result.text))
-  .catch(err => console.error(err));
+            document.getElementById('startButton').addEventListener('click', () => {
+                codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => {
+                    if (result) {
+                        console.log(result)
+                        document.getElementById('result').textContent = result.text
+                    }
+                    if (err && !(err instanceof NotFoundException)) {
+                        console.error(err)
+                        document.getElementById('result').textContent = err
+                    }
+                })
+                console.log(`Started continous decode from camera with id ${selectedDeviceId}`)
+            })
+
+            document.getElementById('resetButton').addEventListener('click', () => {
+                codeReader.reset()
+                document.getElementById('result').textContent = '';
+                console.log('Reset.')
+            })
+
+        })
+        .catch((err) => {
+            console.error(err)
+        })
+})
+
